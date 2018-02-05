64e78943250f3407ad5a69175a90a5b587e89a27
Chaitanya Giri <girichaitanya11@gmail.com>
2018-02-06 00:40:34 +0530

    first commit

---
+++ action.js
@@ -0,0 +1,21 @@
+document.getElementById('fillForm').addEventListener('click', function(){
+    chrome.tabs.executeScript({
+      code: ";"
+    });
+  });
+  document.getElementById('fillForm').addEventListener('click', function(){
+    chrome.tabs.executeScript({
+      file: "myscript.js"
+    });
+  });
+  document.getElementById('fillForm').click();
+
+  
+  // chrome.tabs.onUpdated.addEventListener(function(tabId, changeInfo, tab){
+  //   if(changeInfo.status == 'complete'){
+  //     document.getElementById('LoginUserPassword_auth_username').value = '16ucc028';
+  //     document.getElementById('LoginUserPassword_auth_password').value = 'LNM@LF72';
+  //     document.querySelector('input[type="submit"]').click();
+  //   }    
+    
+  // });
---
+++ myscript.js
@@ -0,0 +1,13 @@
+while(1){
+document.getElementById('LoginUserPassword_auth_username').value = 'YOUR USERNAME';
+document.getElementById('LoginUserPassword_auth_password').value = 'YOUR PASSWORD';
+document.querySelector('input[type="submit"]').click();
+
+var myTimer = setTimeout(function(){
+    document.getElementById('UserCheck_Logoff_Button_span').click();
+    location='Reset';
+    // document.location.reload();
+    console.log("it should appear after 5 second");
+}, 10000);
+}
+
