ac069907cf1e4329dc960edb2615303fa30f887b
Chaitanya Giri <girichaitanya11@gmail.com>
2018-02-06 13:10:13 +0530

    Timeout set for 3 hours and iterations optimized

--- myscript.js
+++ myscript.js
@@ -1,22 +1,19 @@
 var counter = 0;
 var myTimer,myInterval;
 
-for(var i = 0; i<1000000;i++){
+myTimer = setInterval(function myFn(){
     if(document.getElementById('LoginUserPassword_auth_password')  != null){
         document.getElementById('LoginUserPassword_auth_username').value = 'YOUR USERNAME';
         document.getElementById('LoginUserPassword_auth_password').value = 'YOUR PASSWORD';
-        document.querySelector('input[type="submit"]').click();
-    }
-    
-myTimer = setTimeout(function(){
+        document.querySelector('input[type="submit"]').click();        
+    }    
+
     if(document.getElementById('UserCheck_Logoff_Button_span')!= null){
-        document.getElementById('UserCheck_Logoff_Button_span').click();
-        location='Reset';
-    }   
-    console.log("it should appear after 5 second");
-
-}, 5000);
-
-}
-
+        setTimeout(function myFn(){
+            document.getElementById('UserCheck_Logoff_Button_span').click();
+            location='Reset';             
+        console.log("it should appear after 5 second");
+        }, 10800000);
+    }         
+}, 100);
 
