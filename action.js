f9d7fed60929494181a108cc6ea9926a74a80357
Chaitanya Giri <16ucc028@lnmiit.ac.in>
2018-02-07 22:57:05 +0530

    updated

--- action.js
+++ action.js
@@ -11,12 +11,4 @@ document.getElementById('fillForm').addEventListener('click', function(){
   
   document.getElementById('fillForm').click();
 
-  
-  // chrome.tabs.onUpdated.addEventListener(function(tabId, changeInfo, tab){
-  //   if(changeInfo.status == 'complete'){
-  //     document.getElementById('LoginUserPassword_auth_username').value = '16ucc028';
-  //     document.getElementById('LoginUserPassword_auth_password').value = 'LNM@LF72';
-  //     document.querySelector('input[type="submit"]').click();
-  //   }    
-    
-  // });
+ 
